414bc24c36851877b4d1258ce0c7bf807eaef6d0
Evgeny Iakovlev <eyakovlev3@gmail.com>
2020-03-30 19:41:33 +0200

    implement very basic io and pci access
    Signed-off-by: Evgeny Iakovlev <eyakovlev3@gmail.com>

---
+++ include/io.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "types.h"
+
+static inline void out32(uint16_t port, uint32_t val)
+{
+    __asm__ volatile("out %%eax, %%dx" ::"a"(val), "d"(port):);
+}
+
+static inline uint32_t in32(uint16_t port)
+{
+    uint32_t res;
+    __asm__ volatile("in %%dx, %%eax" :"=a"(res) :"d"(port):);
+    return res;
+}
---
+++ include/pci.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "types.h"
+#include "io.h"
+
+#define PCI_CONFADDR ((uint16_t)0x0CF8)
+#define PCI_CONFDATA ((uint16_t)0x0CFC)
+
+static inline uint32_t pci_make_bdf(uint8_t bus, uint8_t dev, uint8_t func)
+{
+    return 0x80000000 | ((uint32_t)(func & 0x7) << 8) | ((uint32_t)(dev & 0x1F) << 11) | ((uint32_t)bus << 16);
+}
+
+static inline uint8_t pci_read8(uint32_t bdf, uint8_t reg)
+{
+    out32(PCI_CONFADDR, bdf | reg);
+    return in32(PCI_CONFDATA) & 0xFF;
+}
+
+static inline uint16_t pci_read16(uint32_t bdf, uint8_t reg)
+{
+    out32(PCI_CONFADDR, bdf | reg);
+    return in32(PCI_CONFDATA) & 0xFFFF;
+}
+
+static inline uint32_t pci_read32(uint32_t bdf, uint8_t reg)
+{
+    out32(PCI_CONFADDR, bdf | reg);
+    return in32(PCI_CONFDATA);
+}
+
+static inline void pci_write32(uint32_t bdf, uint8_t reg, uint32_t val)
+{
+    out32(PCI_CONFADDR, bdf | reg);
+    out32(PCI_CONFDATA, val);
+}
